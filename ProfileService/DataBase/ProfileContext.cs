a7ed8bc92ff6419c2129ee0fa8b19f4152930eed
Manish Kumar Choudhary <ManishKumar.Choudhary@thomsonreuters.com>
2019-12-06 15:25:29 +0530

    Add basic EF modle for profile

---
+++ ProfileService/DataBase/ProfileContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace ProfileService.DataBase
+{
+    public class ProfileContext : System.Data.Entity.DbContext
+    {
+        public ProfileContext() : base(nameOrConnectionString: "Default") { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
