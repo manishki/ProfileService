212d12c38012aa3468949cf43ff970702e70f5ec
Manish Kumar Choudhary <ManishKumar.Choudhary@thomsonreuters.com>
2019-12-06 16:32:00 +0530

    Add basic CRUD operation

--- ProfileService/Controllers/UsersController.cs
+++ ProfileService/Controllers/UsersController.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace ProfileService.Controllers
@@ -11,18 +9,75 @@ namespace ProfileService.Controllers
     public class UsersController : ApiController
     {
         // GET api/users
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            using (var context = new ProfileContext())
+            {
+                var profiles = context.Users.ToArray();
+                return Ok(profiles);
+            }
         }
 
         public IHttpActionResult Get(Guid id)
         {
             using (var context = new ProfileContext())
             {
-                var profile = context.Users.ToArray();
+                var profile = context.Users.Find(id);
                 return Ok(profile);
-            }            
+            }
+        }
+
+        public IHttpActionResult Post(User user)
+        {
+            using (var profile = new ProfileContext())
+            {
+                var newUser = profile.Users.Add(user);
+                profile.SaveChanges();
+                return Ok(newUser);
+            }
+
+        }
+        public IHttpActionResult Put(User user)
+        {
+            using (var profile = new ProfileContext())
+            {
+                var userTobeUpdated = profile.Users.Find(user.Id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    userTobeUpdated.Name = user.Name;
+                    userTobeUpdated.Password = user.Password;
+                    userTobeUpdated.EmailId = user.EmailId;
+                    userTobeUpdated.ContectNumber = user.ContectNumber;
+                    userTobeUpdated.ImageUrl = user.ImageUrl;
+
+                    //user = profile.Users.Add(userTobeUpdated);
+                    profile.SaveChanges();
+                    return Ok(user);
+                }
+
+            }
+        }
+        public IHttpActionResult Delete(Guid id)
+        {
+            using (var profile = new ProfileContext())
+            {
+                var user = profile.Users.Find(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    user = profile.Users.Remove(user);
+                    profile.SaveChanges();
+                    return Ok(user);
+                }
+
+            }
         }
     }
 }
